e777f7b68f6631f1740e0b4187985da959d44d2a
Jarek Sacha <jpsacha@gmail.com>
2020-01-28 21:21:07 -0500

    Move `title` and `icons` properties to the direct scope

--- scalafx-extras-demos/src/main/scala/org/scalafx/extras/mvcfx/stopwatch/StopWatchApp.scala
+++ scalafx-extras-demos/src/main/scala/org/scalafx/extras/mvcfx/stopwatch/StopWatchApp.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2018, ScalaFX Project
+ * Copyright (c) 2011-2020, ScalaFX Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -43,9 +43,9 @@ import scala.language.implicitConversions
 object StopWatchApp extends JFXApp {
 
   stage = new PrimaryStage {
+    icons += new Image("/org/scalafx/extras/sfx.png")
+    title = "StopWatch"
     scene = new Scene {
-      icons += new Image("/org/scalafx/extras/sfx.png")
-      title = "StopWatch"
       root = new BorderPane {
         center = new StopWatch().view
       }
