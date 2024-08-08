8875e1c5489e961d2cf3e965810a437087c6a6e6
Jarek Sacha <jpsacha@gmail.com>
2024-08-08 19:36:44 -0400

    Add demo of image rotation

--- scalafx-extras-demos/src/main/scala/org/scalafx/extras/image/ImageDisplayDemoApp.scala
+++ scalafx-extras-demos/src/main/scala/org/scalafx/extras/image/ImageDisplayDemoApp.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2022, ScalaFX Project
+ * Copyright (c) 2011-2024, ScalaFX Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,6 +32,7 @@ import org.scalafx.extras.ShowMessage
 import scalafx.Includes.*
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.collections.ObservableBuffer
 import scalafx.scene.Scene
 import scalafx.scene.control.*
 import scalafx.scene.image.Image
@@ -42,6 +43,8 @@ import scalafx.stage.FileChooser
  * Demonstrates use of `ImageDisplay` class.
  */
 object ImageDisplayDemoApp extends JFXApp3 {
+
+  private val rotationItems = ObservableBuffer(0, 90, 180, 270)
 
   override def start(): Unit = {
 
@@ -68,6 +71,13 @@ object ImageDisplayDemoApp extends JFXApp3 {
               },
               new ToggleButton("Zoom to fit") {
                 selected <==> imageDisplay.zoomToFit
+              },
+              new ChoiceBox(rotationItems) {
+                selectionModel().selectedItem.onChange { (_, _, newValue) =>
+                  println(s"$newValue chosen in ChoiceBox")
+                  imageDisplay.rotation = newValue
+                }
+                selectionModel().selectFirst()
               }
             )
           }
